bf21c27c80331057af9f2e79a6388262c887af13
Ling_2121 <2609344951@qq.com>
2019-07-11 22:57:14 +0800

    hi

--- Cat/module/graphics/base_graphics.lua
+++ Cat/module/graphics/base_graphics.lua
@@ -5,7 +5,8 @@ local base_graphics = cat.class("base_graphics",pnode){
     color = nil,
 }
 
-function base_graphics:__init__(fill_mode)
+function base_graphics:__init__(fill_mode,x,y)
+    pnode.__init__(self,x,y)
     self.fill_mode = fill_mode or "line"
     self.color = cat.color(255,255,255,255)
 end
@@ -15,6 +16,8 @@ function base_graphics:set_color(r,g,b,a)
     self.color.g = r or  self.color.g
     self.color.b = r or  self.color.b
     self.color.a = r or  self.color.a
+
+    return self
 end
 
 return base_graphics
--- Cat/module/graphics/rectangle.lua
+++ Cat/module/graphics/rectangle.lua
@@ -4,3 +4,18 @@ local rectangle = cat.class("rectangle",baseg){
     width = 10,
     height = 10,
 }
+
+function rectangle:__init__(fill_mode,x,y,w,h)
+    baseg.__init__(self,fill_mode,x,y)
+    self.width = w or 10
+    self.height = h or 10
+end
+
+function rectangle:draw()
+    love.graphics.setColor(self.color:unpack())
+    love.graphics.rectangle(self.fill_mode,self.position.x,self.position.y,self.width,self.height)
+    love.graphics.setColor(255,255,255,255)
+end
+
+
+return rectangle
